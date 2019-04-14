d338be80bbe819bdbcf8156ae18a412d38b36b13
t1732 <oo1732oo@gmail.com>
2019-04-14 13:37:56 +0900

    [feat] add Creadit component

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,15 +5,29 @@ v-app(dark)
   v-content
     v-container
       nuxt
-  v-footer(fixed app)
-    span &copy; 2019
+  v-footer(app color="secondary" height="auto")
+    v-card.footerCard.text-xs-center(flat tile)
+      v-card-text
+        credit
+      v-divider
+      v-card-text.text-xs-center(v-html="copylight")
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
-@Component
+@Component({
+  components: {
+    Credit: () => import('~/components/Credit.vue')
+  }
+})
 export default class Default extends Vue {
   title: string = 'DQB2自己紹介カード作成ツール'
+  copylight: string = '&copy; 2019 - @euphton'
 }
 </script>
+
+<style lang="styl">
+.footerCard
+  width 100%
+</style>
