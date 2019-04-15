15e9db2977c215d75a0a10e42272b2ba9173b711
t1732 <oo1732oo@gmail.com>
2019-04-15 11:06:12 +0900

    [refactor] updated lisence and credit.
    配置とかを変更

---
+++ components/Lisence.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.text-xs-center.mx-3(:class="{ caption: xsSize }")
+  .lisence(v-text="lisenceMessage")
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Lisence extends Vue {
+  lisenceMessage: string = "このページでは、株式会社スクウェア・エニックスが権利を所有する著作物を利用しております。当該著作物の転載・配布は禁止いたします。"
+
+  get xsSize(): boolean {
+    return this.$vuetify.breakpoint.name === 'xs'
+  }
+}
+</script>
+
+<style lang="styl" scoped>
+.lisence
+  font-size 0.8em
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,14 @@ v-app(dark)
   v-toolbar(fixed app)
     v-toolbar-title(v-text="title")
   v-content
+    .mt-3
+      lisence
     v-container
       nuxt
-    .mb-3
-      credit
   v-footer(app color="secondary" height="auto")
     v-card.footerCard.text-xs-center(flat tile)
-      v-card-text.text-xs-center(v-html="copylight")
+      v-card-text.text-xs-center.pt-2.pb-1(v-html="credit_1")
+      v-card-text.text-xs-center.pb-2.pt-0(v-html="credit_2")
 </template>
 
 <script lang="ts">
@@ -17,16 +18,18 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
   components: {
-    Credit: () => import('~/components/Credit.vue')
+    Lisence: () => import('~/components/Lisence.vue')
   }
 })
 export default class Default extends Vue {
   title: string = 'DQB2自己紹介カード作成ツール'
-  copylight: string = '&copy; 2019 - @euphton'
+  credit_1: string = '&copy; 2019 @euphton'
+  credit_2: string = "&copy; 2018 ARMOR PROJECT/BIRD STUDIO/SQUARE ENIX All Rights Reserved."
 }
 </script>
 
 <style lang="styl">
 .footerCard
   width 100%
+  font-size 0.9em
 </style>
